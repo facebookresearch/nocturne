2b2a40092767b41a8cb27c5abb54fc51540d5766
Eugene Vinitsky <eugenevinitsky@Eugenes-MacBook-Pro.local>
2022-10-09 12:57:15 -0700

    switch to both single and multiagent fps

--- scripts/data_analysis/speed_test.py
+++ scripts/data_analysis/speed_test.py
@@ -26,25 +26,30 @@ def run_speed_test(files, cfg):
         [np.float], [np.float]: List of expert accels, list of number
                                 of moving vehicles in file
     """
+    # single agent computation
     times_list = np.zeros(200)
     count_list = np.zeros(200)
     speed_for_ten_vehs = []
     for file in files:
-        for t in range(80):
+        for t in range(89):
             local_cfg = get_scenario_dict(cfg)
             local_cfg['start_time'] = t
             sim = Simulation(os.path.join(PROCESSED_TRAIN_NO_TL, file),
                             local_cfg)
             vehs = sim.scenario().getObjectsThatMoved()
             scenario = sim.getScenario()
+            rand_val = np.random.randint(len(vehs))
+            for i, veh in enumerate(vehs):
+                if i != rand_val:
+                    veh.expert_control = True
             t = time.perf_counter()
-            for veh in vehs:
-                _ = scenario.flattened_visible_state(veh, 80, (120 / 180) * np.pi)
-                veh.apply_action(Action(1.0, 1.0, 1.0))
+            _ = scenario.flattened_visible_state(vehs[rand_val], 80, (120 / 180) * np.pi)
+            veh.apply_action(Action(1.0, 1.0, 1.0))
             sim.step(0.1)
+            total_time = time.perf_counter() - t
             if len(sim.scenario().getVehicles())  > 0 :
-                times_list[len(sim.scenario().getVehicles()) - 1] += (time.perf_counter() - t)
-                count_list[len(sim.scenario().getVehicles()) - 1] += len(vehs)
+                times_list[len(sim.scenario().getVehicles()) - 1] += total_time
+                count_list[len(sim.scenario().getVehicles()) - 1] += 1
                 if len(sim.scenario().getVehicles()) == 10:
                     speed_for_ten_vehs.append((time.perf_counter() - t) / len(vehs))
 
@@ -56,9 +61,42 @@ def run_speed_test(files, cfg):
     plt.figure(dpi=300)
     plt.plot(np.linspace(1, 40, 40), (1 / (times_list / count_list))[0:40])
     plt.xlabel('Number of vehicles in scene')
-    plt.ylabel('FPS')
+    plt.ylabel('steps-per-second')
     print('saving a figure in {}'.format(os.getcwd()))
     plt.savefig('fps_v_agent')
+
+    # multi-agent computation
+    avg_agent_num = []
+    times_list = np.zeros(200)
+    count_list = np.zeros(200)
+    for file in files:
+        for t in range(89):
+            local_cfg = get_scenario_dict(cfg)
+            local_cfg['start_time'] = t
+            sim = Simulation(os.path.join(PROCESSED_TRAIN_NO_TL, file),
+                            local_cfg)
+            vehs = sim.scenario().getObjectsThatMoved()
+            avg_agent_num.append(len(vehs))
+            scenario = sim.getScenario()
+            t = time.perf_counter()
+            for veh in vehs:
+                _ = scenario.flattened_visible_state(veh, 80, (120 / 180) * np.pi)
+                veh.apply_action(Action(1.0, 1.0, 1.0))
+            sim.step(0.1)
+            total_time = time.perf_counter() - t
+            if len(sim.scenario().getVehicles())  > 0 :
+                times_list[len(sim.scenario().getVehicles()) - 1] += total_time
+                count_list[len(sim.scenario().getVehicles()) - 1] += 1
+
+    print(1 / (times_list / count_list))
+    print((1 / (times_list / count_list))[9])
+    plt.figure(dpi=300)
+    plt.plot(np.linspace(1, 40, 40), (1 / (times_list / count_list))[0:40])
+    plt.xlabel('Number of vehicles in scene')
+    plt.ylabel('steps-per-second')
+    print('saving a figure in {}'.format(os.getcwd()))
+    print('the average number of controlled agents is {}'.format(np.mean(avg_agent_num)))
+    plt.savefig('fps_v_agent_ma')
 
 
 @hydra.main(config_path="../../cfgs/", config_name="config")
