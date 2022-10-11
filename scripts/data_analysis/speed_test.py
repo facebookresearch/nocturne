6d09e80d542129ada18f238f8d7c0b702a6c2ec6
Xiaomeng Yang <bit.yangxm@gmail.com>
2022-10-10 19:57:23 -0700

    Group multi-agent stats by number of agents

--- scripts/data_analysis/speed_test.py
+++ scripts/data_analysis/speed_test.py
@@ -12,8 +12,197 @@ import hydra
 import numpy as np
 import matplotlib.pyplot as plt
 
+from typing import Any, Dict, Sequence, Union
+
 from cfgs.config import PROCESSED_TRAIN_NO_TL, get_scenario_dict, set_display_window
 from nocturne import Simulation, Action
+
+MAX_NUM_VEHICLES = 400
+
+
+def single_agent_test(cfg: Dict[str,
+                                Any], files: Sequence[str], num_files: int,
+                      num_steps: int) -> Dict[str, Union[float, np.ndarray]]:
+    sec_by_veh = np.zeros(MAX_NUM_VEHICLES, dtype=np.int64)
+    cnt_by_veh = np.zeros(MAX_NUM_VEHICLES, dtype=np.int64)
+    avg_agt_num = []
+    avg_veh_num = []
+
+    cnt = 0
+    for file in files:
+        cnt += 1
+        if cnt >= num_files:
+            break
+
+        for t in range(num_steps):
+            local_cfg = get_scenario_dict(cfg)
+            local_cfg['start_time'] = t
+            sim = Simulation(os.path.join(PROCESSED_TRAIN_NO_TL, file),
+                             local_cfg)
+            scenario = sim.getScenario()
+
+            vehs = scenario.vehicles()
+            agts = scenario.moving_objects()
+            num_vehs = len(vehs)
+            num_agts = len(agts)
+            if num_agts == 0:
+                break
+
+            num_objs = len(scenario.objects())
+            if num_vehs != num_objs:
+                print(f"[Single] num_vehs = {num_vehs}, num_objs = {num_objs}")
+
+            avg_agt_num.append(num_agts)
+            avg_veh_num.append(num_vehs)
+
+            agt_idx = np.random.randint(num_agts)
+            for i, agt in enumerate(agts):
+                if i != agt_idx:
+                    agt.expert_control = True
+            agt = agts[agt_idx]
+
+            try:
+                t = time.perf_counter_ns()
+                _ = scenario.flattened_visible_state(agt, 80,
+                                                     (120 / 180) * np.pi)
+                agt.apply_action(Action(1.0, 1.0, 1.0))
+                sim.step(0.1)
+                total_time = time.perf_counter_ns() - t
+            except Exception as e:
+                print(e)
+                continue
+
+            if num_vehs <= MAX_NUM_VEHICLES:
+                sec_by_veh[num_vehs - 1] += total_time
+                cnt_by_veh[num_vehs - 1] += 1
+
+    sec_by_veh = sec_by_veh * 1e-9
+    avg_sec = sec_by_veh / cnt_by_veh
+    avg_fps = 1.0 / avg_sec
+    avg_sec = np.nan_to_num(avg_sec)
+    avg_fps = np.nan_to_num(avg_fps)
+
+    overall_avg_sec = np.sum(sec_by_veh) / np.sum(cnt_by_veh)
+    overall_avg_fps = 1.0 / overall_avg_sec
+    avg_agt_num = np.mean(avg_agt_num)
+    avg_veh_num = np.mean(avg_veh_num)
+
+    print(f"[single] avg_sec = {avg_sec}")
+    print(f"[single] avg_fps = {avg_fps}")
+    print(f"[single] overall_avg_sec = {overall_avg_sec}")
+    print(f"[single] overall_avg_fps = {overall_avg_fps}")
+    print(f"[single] overall_avg_veh = {avg_veh_num}")
+    print(f"[single] overall_avg_agt = {avg_agt_num}")
+
+    return {
+        "single_sec_by_veh": sec_by_veh,
+        "single_cnt_by_veh": cnt_by_veh,
+        "single_avg_sec": avg_sec,
+        "single_avg_fps": avg_fps,
+        "single_avg_agt_num": avg_agt_num,
+        "single_avg_veh_num": avg_veh_num,
+    }
+
+
+def multi_agent_test(cfg: Dict[str, Any], files: Sequence[str], num_files: int,
+                     num_steps: int) -> Dict[str, Union[float, np.ndarray]]:
+    sec_by_veh = np.zeros(MAX_NUM_VEHICLES, dtype=np.int64)
+    cnt_by_veh = np.zeros(MAX_NUM_VEHICLES, dtype=np.int64)
+    sec_by_agt = np.zeros(MAX_NUM_VEHICLES, dtype=np.int64)
+    cnt_by_agt = np.zeros(MAX_NUM_VEHICLES, dtype=np.int64)
+    veh_by_agt = np.zeros(MAX_NUM_VEHICLES, dtype=np.int64)
+
+    avg_agt_num = []
+    avg_veh_num = []
+
+    cnt = 0
+    for file in files:
+        cnt += 1
+        if cnt >= num_files:
+            break
+
+        for t in range(num_steps):
+            local_cfg = get_scenario_dict(cfg)
+            local_cfg['start_time'] = t
+            sim = Simulation(os.path.join(PROCESSED_TRAIN_NO_TL, file),
+                             local_cfg)
+            scenario = sim.scenario()
+            vehs = scenario.vehicles()
+            agts = scenario.moving_objects()
+            num_vehs = len(vehs)
+            num_agts = len(agts)
+            if num_agts == 0:
+                break
+
+            num_objs = len(scenario.objects())
+            if num_vehs != num_objs:
+                print(f"[Multi] num_vehs = {num_vehs}, num_objs = {num_objs}")
+
+            avg_agt_num.append(num_agts)
+            avg_veh_num.append(num_vehs)
+
+            try:
+                t = time.perf_counter_ns()
+                for agt in agts:
+                    _ = scenario.flattened_visible_state(
+                        agt, 80, (120 / 180) * np.pi)
+                    agt.apply_action(Action(1.0, 1.0, 1.0))
+                sim.step(0.1)
+                total_time = time.perf_counter_ns() - t
+            except Exception as e:
+                print(e)
+                continue
+
+            if num_vehs <= MAX_NUM_VEHICLES:
+                sec_by_veh[num_vehs - 1] += total_time
+                cnt_by_veh[num_vehs - 1] += 1
+                sec_by_agt[num_agts - 1] += total_time
+                cnt_by_agt[num_agts - 1] += 1
+                veh_by_agt[num_agts - 1] += num_vehs
+
+    sec_by_veh = sec_by_veh * 1e-9
+    sec_by_agt = sec_by_agt * 1e-9
+    avg_sec_by_veh = sec_by_veh / cnt_by_veh
+    avg_sec_by_agt = sec_by_agt / cnt_by_agt
+    avg_fps_by_veh = 1.0 / avg_sec_by_veh
+    avg_fps_by_agt = 1.0 / avg_sec_by_agt
+    avg_veh_by_agt = veh_by_agt / cnt_by_agt
+
+    avg_sec_by_veh = np.nan_to_num(avg_sec_by_veh)
+    avg_fps_by_veh = np.nan_to_num(avg_fps_by_veh)
+    avg_sec_by_agt = np.nan_to_num(avg_sec_by_agt)
+    avg_fps_by_agt = np.nan_to_num(avg_fps_by_agt)
+    avg_veh_by_agt = np.nan_to_num(avg_veh_by_agt)
+
+    overall_avg_sec = np.sum(sec_by_veh) / np.sum(cnt_by_veh)
+    overall_avg_fps = 1.0 / overall_avg_sec
+    avg_agt_num = np.mean(avg_agt_num)
+    avg_veh_num = np.mean(avg_veh_num)
+
+    print(f"[multi] avg_sec_by_veh = {avg_sec_by_veh}")
+    print(f"[multi] avg_fps_by_veh = {avg_fps_by_veh}")
+    print(f"[multi] avg_sec_by_agt = {avg_sec_by_agt}")
+    print(f"[multi] avg_fps_by_agt = {avg_fps_by_agt}")
+    print(f"[multi] avg_veh_by_agt = {avg_veh_by_agt}")
+    print(f"[multi] overall_avg_sec = {overall_avg_sec}")
+    print(f"[multi] overall_avg_fps = {overall_avg_fps}")
+    print(f"[multi] overall_avg_veh = {avg_veh_num}")
+    print(f"[multi] overall_avg_agt = {avg_agt_num}")
+
+    return {
+        "multi_sec_by_veh": sec_by_veh,
+        "multi_cnt_by_veh": cnt_by_veh,
+        "multi_sec_by_agt": sec_by_agt,
+        "multi_cnt_by_agt": cnt_by_agt,
+        "multi_veh_by_agt": veh_by_agt,
+        "multi_avg_sec_by_veh": avg_sec_by_veh,
+        "multi_avg_fps_by_veh": avg_fps_by_veh,
+        "multi_avg_sec_by_agt": avg_sec_by_agt,
+        "multi_avg_fps_by_agt": avg_fps_by_agt,
+        "multi_avg_veh_by_agt": avg_veh_by_agt,
+        "multi_avg_agt_num": avg_agt_num,
+        "multi_avg_veh_num": avg_veh_num,
+    }
 
 
 def run_speed_test(files, cfg):
@@ -28,157 +217,17 @@ def run_speed_test(files, cfg):
                                 of moving vehicles in file
     """
 
-    max_num_vehicles = 200
-    num_time_steps = 90
-    num_files = 10
+    num_files = 1000
+    num_steps = 90
     stats = {}
 
-    # single agent computation
-    times_list = np.zeros(max_num_vehicles)
-    count_list = np.zeros(max_num_vehicles)
+    stats1 = single_agent_test(cfg, files, num_files, num_steps)
+    stats.update(stats1)
 
-    cnt = 0
-    for file in files:
-        cnt += 1
-        if cnt >= num_files:
-            break
+    stats2 = multi_agent_test(cfg, files, num_files, num_steps)
+    stats.update(stats2)
 
-        for t in range(num_time_steps):
-            local_cfg = get_scenario_dict(cfg)
-            local_cfg['start_time'] = t
-            sim = Simulation(os.path.join(PROCESSED_TRAIN_NO_TL, file),
-                             local_cfg)
-            scenario = sim.getScenario()
-
-            vehs = scenario.vehicles()
-            moving_vehs = scenario.moving_objects()
-            num_vehs = len(vehs)
-            num_moving_vehs = len(moving_vehs)
-            if num_moving_vehs == 0:
-                break
-
-            num_objs = len(scenario.objects())
-            if num_vehs != num_objs:
-                print(f"[Single] num_vehs = {num_vehs}, num_objs = {num_objs}")
-
-            agent_idx = np.random.randint(num_moving_vehs)
-            for i, veh in enumerate(moving_vehs):
-                if i != agent_idx:
-                    veh.expert_control = True
-            veh = moving_vehs[agent_idx]
-
-            t = time.perf_counter_ns()
-            _ = scenario.flattened_visible_state(veh, 80, (120 / 180) * np.pi)
-            veh.apply_action(Action(1.0, 1.0, 1.0))
-            sim.step(0.1)
-            total_time = time.perf_counter_ns() - t
-
-            times_list[num_vehs - 1] += total_time
-            count_list[num_vehs - 1] += 1
-
-    times_list *= 1e-9
-    avg_sec = times_list / count_list
-    avg_fps = 1.0 / avg_sec
-    avg_sec = np.nan_to_num(avg_sec)
-    avg_fps = np.nan_to_num(avg_fps)
-
-    print(avg_sec)
-    print(avg_fps)
-
-    overall_avg_sec = np.sum(times_list) / np.sum(count_list)
-    overall_avg_fps = 1.0 / overall_avg_sec
-    print(f"overall_avg_sec = {overall_avg_sec}")
-    print(f"overall_avg_fps = {overall_avg_fps}")
-
-    stats["single_times_list"] = times_list
-    stats["single_count_list"] = count_list
-    stats["single_avg_sec"] = avg_sec
-    stats["single_avg_fps"] = avg_fps
-
-    # print(1 / (times_list / count_list))
-    # print((1 / (times_list / count_list))[9])
-    # print(count_list[9])
-    # print(
-    #     'avg, std. time to get obs for scenes containing 10 vehicles is {}, {}'
-    #     .format(np.mean(1 / np.array(speed_for_ten_vehs)),
-    #             np.std(1 / np.array(speed_for_ten_vehs))))
-    # plt.figure(dpi=300)
-    # plt.plot(np.linspace(1, 40, 40), (1 / (times_list / count_list))[0:40])
-    # plt.xlabel('Number of vehicles in scene')
-    # plt.ylabel('steps-per-second')
-    # print('saving a figure in {}'.format(os.getcwd()))
-    # plt.savefig('fps_v_agent')
-
-    # multi-agent computation
-    times_list = np.zeros(max_num_vehicles)
-    count_list = np.zeros(max_num_vehicles)
-
-    avg_agent_num = []
-    cnt = 0
-    for file in files:
-        cnt += 1
-        if cnt >= num_files:
-            break
-
-        for t in range(num_time_steps):
-            local_cfg = get_scenario_dict(cfg)
-            local_cfg['start_time'] = t
-            sim = Simulation(os.path.join(PROCESSED_TRAIN_NO_TL, file),
-                             local_cfg)
-            scenario = sim.scenario()
-            vehs = scenario.vehicles()
-            moving_vehs = scenario.moving_objects()
-            num_vehs = len(vehs)
-            num_moving_vehs = len(moving_vehs)
-            if num_moving_vehs == 0:
-                break
-
-            num_objs = len(scenario.objects())
-            if num_vehs != num_objs:
-                print(f"[Multi] num_vehs = {num_vehs}, num_objs = {num_objs}")
-
-            avg_agent_num.append(num_moving_vehs)
-            t = time.perf_counter_ns()
-            for veh in moving_vehs:
-                _ = scenario.flattened_visible_state(veh, 80,
-                                                     (120 / 180) * np.pi)
-                veh.apply_action(Action(1.0, 1.0, 1.0))
-            sim.step(0.1)
-            total_time = time.perf_counter_ns() - t
-
-            times_list[num_vehs - 1] += total_time
-            count_list[num_vehs - 1] += 1
-
-    times_list *= 1e-9
-    avg_sec = times_list / count_list
-    avg_fps = 1.0 / avg_sec
-    avg_sec = np.nan_to_num(avg_sec)
-    avg_fps = np.nan_to_num(avg_fps)
-
-    print(avg_sec)
-    print(avg_fps)
-
-    overall_avg_sec = np.sum(times_list) / np.sum(count_list)
-    overall_avg_fps = 1.0 / overall_avg_sec
-    print(f"overall_avg_sec = {overall_avg_sec}")
-    print(f"overall_avg_fps = {overall_avg_fps}")
-
-    stats["multi_times_list"] = times_list
-    stats["multi_count_list"] = count_list
-    stats["multi_avg_sec"] = avg_sec
-    stats["multi_avg_fps"] = avg_fps
-    stats["multi_avg_agent_num"] = np.mean(avg_agent_num)
-
-    # print(1 / (times_list / count_list))
-    # print((1 / (times_list / count_list))[9])
-    # plt.figure(dpi=300)
-    # plt.plot(np.linspace(1, 40, 40), (1 / (times_list / count_list))[0:40])
-    # plt.xlabel('Number of vehicles in scene')
-    # plt.ylabel('steps-per-second')
-    # print('saving a figure in {}'.format(os.getcwd()))
-    # print('the average number of controlled agents is {}'.format(
-    #     np.mean(avg_agent_num)))
-    # plt.savefig('fps_v_agent_ma')
+    print(stats)
 
     with open("./perf_stats.pkl", 'wb') as f:
         pickle.dump(stats, f)
@@ -191,6 +240,7 @@ def analyze_accels(cfg):
     with open(os.path.join(f_path, 'valid_files.json')) as file:
         valid_veh_dict = json.load(file)
         files = list(valid_veh_dict.keys())
+    print(f"tot_files = {len(files)}")
     run_speed_test(files, cfg)
 
 
