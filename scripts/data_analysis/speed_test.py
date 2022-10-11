5b4ec3234ed602598e2b5f0498ff0cb7f4223a81
Eugene Vinitsky <eugenevinitsky@Eugenes-MacBook-Pro.local>
2022-10-10 20:12:44 -0700

    minor

--- scripts/data_analysis/speed_test.py
+++ scripts/data_analysis/speed_test.py
@@ -27,43 +27,43 @@ def run_speed_test(files, cfg):
                                 of moving vehicles in file
     """
     # single agent computation
-    times_list = np.zeros(200)
-    count_list = np.zeros(200)
-    speed_for_ten_vehs = []
-    for file in files:
-        for t in range(89):
-            local_cfg = get_scenario_dict(cfg)
-            local_cfg['start_time'] = t
-            sim = Simulation(os.path.join(PROCESSED_TRAIN_NO_TL, file),
-                            local_cfg)
-            vehs = sim.scenario().getObjectsThatMoved()
-            scenario = sim.getScenario()
-            rand_val = np.random.randint(len(vehs))
-            for i, veh in enumerate(vehs):
-                if i != rand_val:
-                    veh.expert_control = True
-            t = time.perf_counter()
-            _ = scenario.flattened_visible_state(vehs[rand_val], 80, (120 / 180) * np.pi)
-            veh.apply_action(Action(1.0, 1.0, 1.0))
-            sim.step(0.1)
-            total_time = time.perf_counter() - t
-            if len(sim.scenario().getVehicles())  > 0 :
-                times_list[len(sim.scenario().getVehicles()) - 1] += total_time
-                count_list[len(sim.scenario().getVehicles()) - 1] += 1
-                if len(sim.scenario().getVehicles()) == 10:
-                    speed_for_ten_vehs.append((time.perf_counter() - t) / len(vehs))
+    # times_list = np.zeros(200)
+    # count_list = np.zeros(200)
+    # speed_for_ten_vehs = []
+    # for file in files:
+    #     for t in range(89):
+    #         local_cfg = get_scenario_dict(cfg)
+    #         local_cfg['start_time'] = t
+    #         sim = Simulation(os.path.join(PROCESSED_TRAIN_NO_TL, file),
+    #                         local_cfg)
+    #         vehs = sim.scenario().getObjectsThatMoved()
+    #         scenario = sim.getScenario()
+    #         rand_val = np.random.randint(len(vehs))
+    #         for i, veh in enumerate(vehs):
+    #             if i != rand_val:
+    #                 veh.expert_control = True
+    #         t = time.perf_counter()
+    #         _ = scenario.flattened_visible_state(vehs[rand_val], 80, (120 / 180) * np.pi)
+    #         veh.apply_action(Action(1.0, 1.0, 1.0))
+    #         sim.step(0.1)
+    #         total_time = time.perf_counter() - t
+    #         if len(sim.scenario().getVehicles())  > 0 :
+    #             times_list[len(sim.scenario().getVehicles()) - 1] += total_time
+    #             count_list[len(sim.scenario().getVehicles()) - 1] += 1
+    #             if len(sim.scenario().getVehicles()) == 10:
+    #                 speed_for_ten_vehs.append((time.perf_counter() - t) / len(vehs))
 
-    print(1 / (times_list / count_list))
-    print((1 / (times_list / count_list))[9])
-    print(count_list[9])
-    print('avg, std. time to get obs for scenes containing 10 vehicles is {}, {}'.format(np.mean(1 / np.array(speed_for_ten_vehs)),
-                                                       np.std(1 / np.array(speed_for_ten_vehs))))
-    plt.figure(dpi=300)
-    plt.plot(np.linspace(1, 40, 40), (1 / (times_list / count_list))[0:40])
-    plt.xlabel('Number of vehicles in scene')
-    plt.ylabel('steps-per-second')
-    print('saving a figure in {}'.format(os.getcwd()))
-    plt.savefig('fps_v_agent')
+    # print(1 / (times_list / count_list))
+    # print((1 / (times_list / count_list))[9])
+    # print(count_list[9])
+    # print('avg, std. time to get obs for scenes containing 10 vehicles is {}, {}'.format(np.mean(1 / np.array(speed_for_ten_vehs)),
+    #                                                    np.std(1 / np.array(speed_for_ten_vehs))))
+    # plt.figure(dpi=300)
+    # plt.plot(np.linspace(1, 40, 40), (1 / (times_list / count_list))[0:40])
+    # plt.xlabel('Number of vehicles in scene')
+    # plt.ylabel('steps-per-second')
+    # print('saving a figure in {}'.format(os.getcwd()))
+    # plt.savefig('fps_v_agent')
 
     # multi-agent computation
     avg_agent_num = []
@@ -71,22 +71,25 @@ def run_speed_test(files, cfg):
     count_list = np.zeros(200)
     for file in files:
         for t in range(89):
-            local_cfg = get_scenario_dict(cfg)
-            local_cfg['start_time'] = t
-            sim = Simulation(os.path.join(PROCESSED_TRAIN_NO_TL, file),
-                            local_cfg)
-            vehs = sim.scenario().getObjectsThatMoved()
-            avg_agent_num.append(len(vehs))
-            scenario = sim.getScenario()
-            t = time.perf_counter()
-            for veh in vehs:
-                _ = scenario.flattened_visible_state(veh, 80, (120 / 180) * np.pi)
-                veh.apply_action(Action(1.0, 1.0, 1.0))
-            sim.step(0.1)
-            total_time = time.perf_counter() - t
-            if len(sim.scenario().getVehicles())  > 0 :
-                times_list[len(sim.scenario().getVehicles()) - 1] += total_time
-                count_list[len(sim.scenario().getVehicles()) - 1] += 1
+            try:
+                local_cfg = get_scenario_dict(cfg)
+                local_cfg['start_time'] = t
+                sim = Simulation(os.path.join(PROCESSED_TRAIN_NO_TL, file),
+                                local_cfg)
+                vehs = sim.scenario().getObjectsThatMoved()
+                avg_agent_num.append(len(vehs))
+                scenario = sim.getScenario()
+                t = time.perf_counter()
+                for veh in vehs:
+                    _ = scenario.flattened_visible_state(veh, 80, (120 / 180) * np.pi)
+                    veh.apply_action(Action(1.0, 1.0, 1.0))
+                sim.step(0.1)
+                total_time = time.perf_counter() - t
+                if len(sim.scenario().getVehicles())  > 0 :
+                    times_list[len(sim.scenario().getVehicles()) - 1] += total_time
+                    count_list[len(sim.scenario().getVehicles()) - 1] += 1
+            except:
+                import ipdb; ipdb.set_trace()
 
     print(1 / (times_list / count_list))
     print((1 / (times_list / count_list))[9])
