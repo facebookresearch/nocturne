65eb21dc0a8c3dcdd9e1ef7a9578c7111fdcacf9
Eugene Vinitsky <eugenevinitsky@Eugenes-MacBook-Pro.local>
2022-10-12 06:35:39 -0700

    plot script

--- scripts/data_analysis/speed_test_analysis.py
+++ scripts/data_analysis/speed_test_analysis.py
@@ -2,17 +2,16 @@ import pickle as pkl
 import numpy as np
 import matplotlib.pyplot as plt
 
-stats_dict = np.load('perf_stats_1000.pkl', allow_pickle=True)
+stats_dict = np.load('perf_stats_1000_new.pkl', allow_pickle=True)
+import ipdb; ipdb.set_trace()
 plt.figure(dpi=300)
 plt.plot(stats_dict['single_avg_fps'][0:50])
-plt.xlabel('Number of vehicles in the scene')
+plt.xlabel('Number of non-agent vehicles in the scene')
 plt.ylabel('Steps-per-second')
 plt.savefig('sa_sps.png')
 
-stats_dict = np.load('perf_stats_1000_multi_agent_only.pkl', allow_pickle=True)
-import ipdb; ipdb.set_trace()
 plt.figure(dpi=300)
-plt.plot(stats_dict['multi_avg_sec_by_agt'][0:50])
+plt.plot(np.linspace(1, 49, 50), stats_dict['multi_avg_sec_by_agt'][0:50])
 plt.xlabel('Number of controlled agents')
 plt.ylabel('Time per step (s)')
 plt.savefig('ma_time_per_step.png')
