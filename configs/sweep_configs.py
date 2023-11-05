f074052027ce5753b49d392548c0a61a1b7300ba
Daphne Cornelisse <cor.daphne@gmail.com>
2023-11-05 14:54:04 -0500

    Restructure and minor updates.

--- configs/sweep_configs.py
+++ configs/sweep_configs.py
@@ -1,17 +1,17 @@
 sweep_config_arch = {
-    'project': 'ppo_scaling',
+    'project': 'ppo_scaling_2_scenes',
     'name': 'mlp_archs',
     'metric': {'name': 'rollout/ep_rew_mean_norm', 'goal':'maximize'},
     'method': 'random',
     'parameters': {
         'total_timesteps': {'values': [10_000_000]},
-        'policy_layers': {'values': [[512, 256, 64], [1048, 512, 128], [64, 64]]},
+        'policy_layers': {'values': [[512, 256, 64], [64, 64]]},
         'policy_act_func': {'values': ['tanh', 'relu']},
         'memory': {'values': [1]},
         'ent_coef': {'values': [0, 0.025, 0.05]},
-        'vf_coef': {'values': [0.5, 0.25]},
-        'num_controlled_agents': {'values': [3, 4, 5]},
-        'seed': {'values': [42, 6, 0]},
+        'vf_coef': {'values': [0.5]},
+        'num_controlled_agents': {'values': [5]},
+        'seed': {'values': [42, 6]},
     }
 }
 
--- experiments/rl/ppo_sweep.py
+++ experiments/rl/ppo_sweep.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import torch
 import wandb
 
-
 # Multi-agent as vectorized environment
 from nocturne.envs.vec_env_ma import MultiAgentAsVecEnv
 from utils.config import load_config
