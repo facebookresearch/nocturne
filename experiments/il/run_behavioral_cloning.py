c8441d4c0ac01a1a904fda20f93ee3fdacc40957
Daphne Cornelisse <cor.daphne@gmail.com>
2023-11-19 17:55:26 -0500

    IL: Improve setup, train IL policy, save and validate.

--- experiments/il/run_behavioral_cloning.py
+++ experiments/il/run_behavioral_cloning.py
@@ -1,26 +1,36 @@
 import numpy as np
-import torch
 from torch.utils.data import DataLoader
 import wandb
+import torch
+from pathlib import Path
+from datetime import datetime
 
 from imitation.algorithms import bc
 from imitation.data.types import Transitions
+from imitation.policies.serialize import save_stable_model
 
 from utils.wrappers import LightNocturneEnvWrapper
 from utils.config import load_config
 from utils.imitation_learning.waymo_iterator import TrajectoryIterator
 from utils.evaluation import evaluate_policy
 
+from utils.string_utils import date_to_str
 
 if __name__ == "__main__":
 
     # Create run
-    run = wandb.init(project="eval_il_policy")
+    run = wandb.init(
+        project="eval_il_policy",
+        sync_tensorboard=True,
+    )
 
     # Configs
     video_config = load_config("video_config")
     env_config = load_config("env_config")
     bc_config = load_config("bc_config")
+
+    # Device TODO: Add support for CUDA
+    device = "cpu" 
 
     # Create iterator
     waymo_iterator = TrajectoryIterator(
@@ -39,11 +49,11 @@ if __name__ == "__main__":
 
     # Convert to dataset of imitation "transitions" 
     transitions = Transitions(
-        obs=rollouts[0], 
-        acts=rollouts[1], 
-        infos=torch.zeros_like(rollouts[0]), # Dummy
+        obs=rollouts[0].to(device),
+        acts=rollouts[1].to(device), 
+        infos=np.zeros_like(rollouts[0]), # Dummy
         next_obs=rollouts[2],
-        dones=np.array(rollouts[3]).astype(bool)
+        dones=np.array(rollouts[3]).astype(bool),
     )
 
     # Define trainer
@@ -53,18 +63,20 @@ if __name__ == "__main__":
         action_space=waymo_iterator.action_space,
         demonstrations=transitions,
         rng=rng,
-        device="cpu",
+        device=device,
     )
 
     # Create evaluation env
-    env_config.sample_file_method = "no_replacement"
+    env = LightNocturneEnvWrapper(env_config)
+    eval_files = env.files
 
     # Check random behavior
     reward_before_training, _ = evaluate_policy(
         model=bc_trainer.policy, 
         env=LightNocturneEnvWrapper(env_config),
         n_steps_per_episode=env_config.episode_length, 
-        n_eval_episodes=env_config.num_files,
+        n_eval_episodes=1,
+        eval_files=eval_files,
         video_config=video_config,
         video_caption="BEFORE training",
         render=True,
@@ -80,7 +92,8 @@ if __name__ == "__main__":
         model=bc_trainer.policy, 
         env=LightNocturneEnvWrapper(env_config),
         n_steps_per_episode=env_config.episode_length, 
-        n_eval_episodes=env_config.num_files,
+        n_eval_episodes=1,
+        eval_files=eval_files,
         video_config=video_config,
         video_caption=f"AFTER training ({bc_config.n_epochs} epochs)",
         render=True,
@@ -88,3 +101,9 @@ if __name__ == "__main__":
 
     print(f"Reward before training: {reward_before_training:.2f}")
     print(f"Reward after  training: {reward_after_training:.2f}")
+
+    # Save model
+    date_ = date_to_str(datetime.now())
+
+    if bc_config.save_model:
+        bc_trainer.policy.save(path=f"{bc_config.save_model_path}{bc_config.model_name}_{date_}.pt")
--- utils/evaluation.py
+++ utils/evaluation.py
@@ -7,11 +7,13 @@ def evaluate_policy(
     env,
     n_steps_per_episode,
     n_eval_episodes,
+    eval_files=None,
     deterministic = True,
     render = False,
     video_caption = None,
     video_config = None,
     return_episode_rewards = False,
+    verbose=1,
 ):
     """
     Runs policy for ``n_eval_episodes`` episodes and returns average reward.
@@ -33,55 +35,58 @@ def evaluate_policy(
     episode_rewards = np.zeros(n_eval_episodes)
     episode_lengths = np.zeros(n_eval_episodes)
 
-    for episode_i in range(n_eval_episodes):
- 
-        # Reset env
-        observations = env.reset()
-        num_agents_controlled = len(env.agent_ids)
-        curr_rewards = np.zeros(num_agents_controlled)
-        frames = []
-        
-        for step_j in range(n_steps_per_episode):
+    for traffic_scene in eval_files:
+        if verbose == 1:
+            print(f"Evaluating policy on {traffic_scene}...")
 
-            actions, _ = model.predict(
-                observations,
-                deterministic=deterministic,
-            )
-            new_observations, rewards, dones, infos = env.step(actions)
-
-            for agent_idx, agent_id in enumerate(env.agent_ids):
-                if agent_id not in env.dead_agent_ids:
-                    curr_rewards[agent_idx] += rewards[agent_idx]
-
-            observations = new_observations
-
-            # Render
-            if render:
-                if step_j % video_config.logging.render_interval == 0:
-                    if video_config.logging.where_am_i == "headless_machine":
-                        with Display() as disp:
-                            render_scene = env.env.scenario.getImage(**video_config.render)
-                            frames.append(render_scene.T)
-                    else:
-                        render_scene = env.scenario.getImage(**video_config)
-                        frames.append(render_scene.T)
+        for episode_i in range(n_eval_episodes):
+            # Reset env
+            observations = env.reset(filename=traffic_scene)
+            num_agents_controlled = len(env.agent_ids)
+            curr_rewards = np.zeros(num_agents_controlled)
+            frames = []
             
-            if sum(dones) == env.num_agents or step_j == (n_steps_per_episode-1):
-                episode_rewards[episode_i] = curr_rewards.sum() / env.num_agents
-                episode_lengths[episode_i] = step_j
-                break
+            for step_j in range(n_steps_per_episode):
 
-        # Log video to wandb
-        if render:
-            movie_frames = np.array(frames, dtype=np.uint8)
-            video_key = f"Scene (N = {env.num_agents}): {env.env.file}" 
-            wandb.log(
-                {
-                    video_key: wandb.Video(movie_frames, 
-                    fps=video_config.logging.fps, 
-                    caption=f'{video_caption} | norm_ep_return = {episode_rewards[episode_i]:.2f}'),
-                },
-            )
+                actions, _ = model.predict(
+                    observations,
+                    deterministic=deterministic,
+                )
+                new_observations, rewards, dones, infos = env.step(actions)
+
+                for agent_idx, agent_id in enumerate(env.agent_ids):
+                    if agent_id not in env.dead_agent_ids:
+                        curr_rewards[agent_idx] += rewards[agent_idx]
+
+                observations = new_observations
+
+                # Render
+                if render:
+                    if step_j % video_config.logging.render_interval == 0:
+                        if video_config.logging.where_am_i == "headless_machine":
+                            with Display() as disp:
+                                render_scene = env.env.scenario.getImage(**video_config.render)
+                                frames.append(render_scene.T)
+                        else:
+                            render_scene = env.scenario.getImage(**video_config)
+                            frames.append(render_scene.T)
+                
+                if sum(dones) == env.num_agents or step_j == (n_steps_per_episode-1):
+                    episode_rewards[episode_i] = curr_rewards.sum() / env.num_agents
+                    episode_lengths[episode_i] = step_j
+                    break
+
+            # Log video to wandb
+            if render:
+                movie_frames = np.array(frames, dtype=np.uint8)
+                video_key = f"Scene (N = {env.num_agents}): {env.env.file}" 
+                wandb.log(
+                    {
+                        video_key: wandb.Video(movie_frames, 
+                        fps=video_config.logging.fps, 
+                        caption=f'{video_caption} | norm_ep_return = {episode_rewards[episode_i]:.2f}'),
+                    },
+                )
 
     # Close env
     env.close()
