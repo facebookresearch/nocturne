9ec0a7df2585db3c6616dbcb339f910e32b631b9
Alex Wong <alexcw20010923@gmail.com>
2023-11-30 21:47:53 -0800

    feat: Add basic implementation of A3C

--- algos/ppo/r_mappo/r_a3c.py
+++ algos/ppo/r_mappo/r_a3c.py
@@ -46,3 +46,109 @@ class R_A3C():
             self.value_normalizer = ValueNorm(1, device=self.device)
         else:
             self.value_normalizer = None
+
+    def cal_value_loss(self, values, returns, active_masks):
+        """
+        Calculate value function loss.
+        :param values: (torch.Tensor) value function predictions.
+        :param returns: (torch.Tensor) reward to go returns.
+        :param active_masks: (torch.Tensor) denotes if agent is active or dead at a given timesep.
+
+        :return value_loss: (torch.Tensor) value function loss.
+        """
+        if self._use_popart or self._use_valuenorm:
+            self.value_normalizer.update(returns)
+            error_original = self.value_normalizer.normalize(
+                returns) - values
+        else:
+            # advantage function
+            error_original = returns - values
+
+        if self._use_huber_loss:
+            value_loss_original = huber_loss(error_original, self.huber_delta)
+        else:
+            value_loss_original = mse_loss(error_original)
+
+        value_loss = value_loss_original
+
+        if self._use_value_active_masks:
+            value_loss = (value_loss *
+                          active_masks).sum() / active_masks.sum()
+        else:
+            value_loss = value_loss.mean()
+
+        return value_loss
+    
+    def a3c_update(self, sample, update_actor=True):
+        """
+        Update actor and critic networks.
+        :param sample: (Tuple) contains data batch with which to update networks.
+        :update_actor: (bool) whether to update actor network.
+
+        :return value_loss: (torch.Tensor) value function loss.
+        :return critic_grad_norm: (torch.Tensor) gradient norm from critic up9date.
+        ;return policy_loss: (torch.Tensor) actor(policy) loss value.
+        :return dist_entropy: (torch.Tensor) action entropies.
+        :return actor_grad_norm: (torch.Tensor) gradient norm from actor update.
+        :return imp_weights: (torch.Tensor) importance sampling weights.
+        """
+        share_obs_batch, obs_batch, rnn_states_batch, rnn_states_critic_batch, actions_batch, \
+        value_preds_batch, return_batch, masks_batch, active_masks_batch, old_action_log_probs_batch, \
+        adv_targ, available_actions_batch = sample
+
+        old_action_log_probs_batch = check(old_action_log_probs_batch).to(
+            **self.tpdv)
+        adv_targ = check(adv_targ).to(**self.tpdv)
+        # value_preds_batch = check(value_preds_batch).to(**self.tpdv)
+        return_batch = check(return_batch).to(**self.tpdv)
+        active_masks_batch = check(active_masks_batch).to(**self.tpdv)
+
+        # Reshape to do in a single forward pass for all steps
+        values, action_log_probs, dist_entropy = self.policy.evaluate_actions(
+            obs_batch, rnn_states_batch,
+            rnn_states_critic_batch, actions_batch, masks_batch,
+            available_actions_batch, active_masks_batch)
+        # actor update
+        imp_weights = torch.exp(action_log_probs - old_action_log_probs_batch)
+
+        surrogate_loss = imp_weights * adv_targ
+
+        if self._use_policy_active_masks:
+            policy_action_loss = (
+                -torch.sum(surrogate_loss, dim=-1, keepdim=True) *
+                active_masks_batch).sum() / active_masks_batch.sum()
+        else:
+            policy_action_loss = -torch.sum(
+                surrogate_loss, dim=-1, keepdim=True).mean()
+
+        policy_loss = policy_action_loss
+
+        self.policy.actor_optimizer.zero_grad()
+
+        if update_actor:
+            (policy_loss - dist_entropy * self.entropy_coef).backward()
+
+        if self._use_max_grad_norm:
+            actor_grad_norm = nn.utils.clip_grad_norm_(
+                self.policy.actor.parameters(), self.max_grad_norm)
+        else:
+            actor_grad_norm = get_gard_norm(self.policy.actor.parameters())
+
+        self.policy.actor_optimizer.step()
+
+        # critic update
+        value_loss = self.cal_value_loss(values, return_batch, active_masks_batch)
+
+        self.policy.critic_optimizer.zero_grad()
+
+        (value_loss * self.value_loss_coef).backward()
+
+        if self._use_max_grad_norm:
+            critic_grad_norm = nn.utils.clip_grad_norm_(
+                self.policy.critic.parameters(), self.max_grad_norm)
+        else:
+            critic_grad_norm = get_gard_norm(self.policy.critic.parameters())
+
+        self.policy.critic_optimizer.step()
+
+        return value_loss, critic_grad_norm, policy_loss, dist_entropy, actor_grad_norm, imp_weights
