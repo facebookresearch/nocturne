f0a792eb4b2af18ca1af6742bc343392ae992f48
Thang Chau <thangnc@uci.edu>
2023-11-30 20:48:50 -0800

    Added constructor for trainer class

--- algos/ppo/r_mappo/r_a3c.py
+++ algos/ppo/r_mappo/r_a3c.py
@@ -1,1 +1,51 @@
 # Code adapted from https://github.com/facebookresearch/nocturne/blob/main/algos/ppo/r_mappo/r_mappo.py
+import numpy as np
+import torch
+import torch.nn as nn
+from algos.ppo.utils.util import get_gard_norm, huber_loss, mse_loss
+from algos.ppo.utils.valuenorm import ValueNorm
+from algos.ppo.ppo_utils.util import check
+
+
+class R_A3C():
+    """
+    Trainer class for A3C to update policies.
+    :param args: (argparse.Namespace) arguments containing relevant model, policy, and env information.
+    :param policy: (R_A3C_Policy) policy to update.
+    :param device: (torch.device) specifies the device to run on (cpu/gpu).
+    """
+
+    def __init__(self, args, policy, device=torch.device("cpu")):
+
+        self.device = device
+        self.tpdv = dict(dtype=torch.float32, device=device)
+        self.policy = policy
+
+        self.clip_param = args.clip_param
+        self.ppo_epoch = args.ppo_epoch
+        self.num_mini_batch = args.num_mini_batch
+        self.data_chunk_length = args.data_chunk_length
+        self.value_loss_coef = args.value_loss_coef
+        self.entropy_coef = args.entropy_coef
+        self.max_grad_norm = args.max_grad_norm
+        self.huber_delta = args.huber_delta
+
+        self._use_recurrent_policy = args.use_recurrent_policy
+        self._use_naive_recurrent = args.use_naive_recurrent_policy
+        self._use_max_grad_norm = args.use_max_grad_norm
+        self._use_huber_loss = args.use_huber_loss
+        self._use_popart = args.use_popart
+        self._use_valuenorm = args.use_valuenorm
+        self._use_value_active_masks = args.use_value_active_masks
+        self._use_policy_active_masks = args.use_policy_active_masks
+
+        assert (self._use_popart and self._use_valuenorm) == False, (
+            "self._use_popart and self._use_valuenorm can not be set True simultaneously"
+        )
+
+        if self._use_popart:
+            self.value_normalizer = self.policy.critic.v_out
+        elif self._use_valuenorm:
+            self.value_normalizer = ValueNorm(1, device=self.device)
+        else:
+            self.value_normalizer = None
